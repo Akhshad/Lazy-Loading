8614564e2e1c9f56691d96bd6294ee43085e1ff4
Akhshad Gorle <akhshadgorle@gmail.com>
2024-05-10 15:18:50 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loading1';
+
+  constructor(private spinner: NgxSpinnerService) {}
+
+openSpinner() {
+  this.spinner.show(); 
+  const showPeriods = () => {
+  };
+  showPeriods(); 
+  setTimeout(() => {
+    this.spinner.hide(); // Hide the spinner after 5 seconds
+  },7000); 
+}
+
 }
